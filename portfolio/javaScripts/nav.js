7d5dfcbbc4412100e541ae8778307c57f94440a1
Sangpil Jung <59302973+Thinkteam20@users.noreply.github.com>
2021-09-06 11:14:07 +1000

    JS project animation

--- portfolio/javaScripts/nav.js
+++ portfolio/javaScripts/nav.js
@@ -60,3 +60,29 @@ window.addEventListener("scroll", () => {
 arrowBtn.addEventListener("click", (event) => {
   scrollIntoView("#home");
 });
+
+// work - project filterling animation
+
+const workBtnContainer = document.querySelector(".work__categories");
+const projectContainer = document.querySelector(".work__projects");
+const projects = document.querySelectorAll(".project");
+
+// console.log(workBtnContainer);
+workBtnContainer.addEventListener("click", (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if (filter == null) {
+    return;
+  }
+
+  projectContainer.classList.add("anim-out");
+  setTimeout(() => {
+    projects.forEach((project) => {
+      if (filter === "*" || filter === project.dataset.type) {
+        project.classList.remove("invisible");
+      } else {
+        project.classList.add("invisible");
+      }
+    });
+    projectContainer.classList.remove("anim-out");
+  }, 300);
+});
